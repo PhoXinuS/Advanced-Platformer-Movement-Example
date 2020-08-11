f35e62d3f781cafb67a2b30a6d2b6b396bfceddb
PhoXinuS <panhenontre@gmail.com>
2020-08-11 09:22:16 +0200

    Fixed rope

---
+++ project Noir/Assets/Scripts/Trash bin/ForceME.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ForceME : MonoBehaviour
+{
+    [SerializeField] private Vector2 force;
+    [SerializeField] bool apply;
+    private Rigidbody2D rb2D;
+
+    private void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+        
+        if (apply)
+        {
+            rb2D.AddForce(force);
+        }
+    }
+
+    private void Update()
+    {
+        if (apply)
+        {
+            rb2D.AddForce(force);
+        }
+    }
+}
